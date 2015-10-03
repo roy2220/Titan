f09846869025e31d00876d9375bb929d2cff1c83
Roy O'Young <roy2220@outlook.com>
2015-10-03 20:13:15 +0000

    minor

--- Binary.c
+++ Binary.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2015 Roy O'Young <roy2220@outlook.com>.
+ */
+
+
 #include "Binary.h"
 
 #include <assert.h>
--- Binary.h
+++ Binary.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2015 Roy O'Young <roy2220@outlook.com>.
+ */
+
+
 #pragma once
 
 
